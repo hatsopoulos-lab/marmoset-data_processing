3204a7f1dee2daf954180644b7ba48eb1b5e2be8
daltondm <daltonm@uchicago.edu>
2025-02-16 17:49:33 -0600

    updated TEMPLATE_add_phy_sorting_to_nwb.py and did more organization of subject_specific_scripts/MG

--- subject_specific_scripts/TEMPLATES/python/TEMPLATE_add_phy_sorting_to_nwb.py
+++ subject_specific_scripts/TEMPLATES/python/TEMPLATE_add_phy_sorting_to_nwb.py
@@ -7,13 +7,14 @@ Created on Mon Mar 27 08:38:34 2023
 """
 
 from pynwb import NWBHDF5IO
-from neuroconv.datainterfaces import PhySortingInterface
+from neuroconv.datainterfaces import PhySortingInterface, BlackrockRecordingInterface
 import ndx_pose
 from importlib import sys
 import os
+from pathlib import Path
 
 sys.path.insert(0, '/project/nicho/projects/marmosets/code_database/data_processing/nwb_tools/hatlab_nwb_tools/')
-from hatlab_nwb_functions import create_nwb_copy_without_acquisition    
+from hatlab_nwb_functions import create_nwb_copy_without_acquisition, create_nwb_copy_with_external_links_to_acquisition    
 
 '''
     If you are creating the processed file for the first time, you will want to use:
@@ -36,17 +37,27 @@ from hatlab_nwb_functions import create_nwb_copy_without_acquisition
 # import phy curation   '/project/nicho/data/marmosets/electrophys_data_for_processing/TY20210211_freeAndMoths/TY20210211_freeAndMoths-003'
 # base_nwb_file_pattern = '/project/nicho/data/marmosets/electrophys_data_for_processing/TY20210211_freeAndMoths/TY20210211_freeAndMoths-003'
 # phy_path = os.path.join(os.path.dirname(base_nwb_file_pattern), 'phy_IC_2023_06_12') 
-base_nwb_file_pattern = '/project/nicho/data/marmosets/electrophys_data_for_processing/MG20230416_1505_mothsAndFree/MG20230416_1505_mothsAndFree-002'
-phy_path = os.path.join(os.path.dirname(base_nwb_file_pattern), 'phy_IC_DM_2023_06_05') 
+make_new_processed_file = True
 
-nwb_acquisition_file  = base_nwb_file_pattern + '_acquisition.nwb'
-nwb_processed_infile  = base_nwb_file_pattern + '_acquisition.nwb' # base_nwb_file_pattern + '_processed.nwb'
-nwb_processed_outfile = base_nwb_file_pattern + '_processed_resorted_20230612.nwb'
+nwb_acquisition_file = Path('/project/nicho/data/marmosets/electrophys_data_for_processing/TY20210211_freeAndMoths/TY20210211_freeAndMoths-003_acquisition.nwb')
+ns6_file = nwb_acquisition_file.parent / nwb_acquisition_file.name.replace('_acquisition.nwb', '.ns6')
+nwb_storage_folder = 'demo_nwb_files' #'' 
 
-removed_chans = [] #[34, 50, 52, 67, 81]
-# removed_chans = [ch-1 for ch in removed_chans]
-# removed_chans = [29, 33, 49, 51, 66, 80]
-# removed_chans = []
+phy_path = nwb_acquisition_file.parent / 'phy_IC_2023_06_12' 
+
+nwb_processed_outfile = nwb_acquisition_file.parent / nwb_storage_folder / nwb_acquisition_file.name.replace('_acquisition.nwb', '_processed_testing.nwb')
+if make_new_processed_file:
+    nwb_processed_infile = nwb_acquisition_file 
+else:
+    nwb_processed_infile = nwb_processed_outfile
+
+dataset_id = nwb_acquisition_file.parts[-2]
+if dataset_id in ['TY20210211_freeAndMoths']:
+    removed_chans = [29, 33, 49, 51, 66, 80]
+elif dataset_id in ['TY20210211_inHammock_night']:
+    removed_chans = [33, 49]
+elif 'MG2023' in dataset_id:
+    removed_chans = []    
 
 write_options = dict(write_as='units', 
                      units_name='units', 
@@ -56,19 +67,32 @@ with NWBHDF5IO(nwb_acquisition_file, 'r') as io:
     nwb = io.read()
     electrodes = nwb.electrodes.to_dataframe() 
     session_start_time = nwb.session_start_time
-    raw_start_time  = nwb.acquisition['ElectricalSeriesRaw'].starting_time
-    raw_sample_rate = nwb.acquisition['ElectricalSeriesRaw'].rate
+    es_key = [key for key in nwb.acquisition.keys() if 'Electrical' in key][0]
+    raw_start_time  = nwb.acquisition[es_key].starting_time
+    raw_sample_rate = nwb.acquisition[es_key].rate       
     start_idx_modifier = int(raw_start_time * raw_sample_rate)
 
 if nwb_processed_infile != nwb_processed_outfile:
     create_nwb_copy_without_acquisition(nwb_processed_infile, nwb_processed_outfile)
+    # create_nwb_copy_with_external_links_to_acquisition(nwb_processed_infile, nwb_processed_outfile)
 
+# phy_interface = PhySortingInterface(folder_path = phy_path, 
+#                                     exclude_cluster_groups='noise', 
+#                                     start_index_modifier=start_idx_modifier)    
 phy_interface = PhySortingInterface(folder_path = phy_path, 
-                                    exclude_cluster_groups='noise', 
-                                    start_index_modifier=start_idx_modifier)
+                                    exclude_cluster_groups='noise')
+# phy_interface.adjust_spiketrain_indices_for_starting_time(start_idx_modifier)
+source_data = dict(file_path=ns6_file)
+recordingInterface = BlackrockRecordingInterface(**source_data)
+phy_interface.register_recording(recordingInterface)
+# phy_interface.set_aligned_starting_time(aligned_starting_time=raw_start_time)
+# phy_interface.set_aligned_starting_time(aligned_starting_time=raw_start_time)
 phy_extractor = phy_interface.sorting_extractor
 
-
+# phy_interface = PhySortingInterface(folder_path = phy_path, 
+#                                     exclude_cluster_groups='noise', 
+#                                     start_index_modifier=start_idx_modifier)
+# phy_extractor = phy_interface.sorting_extractor
 
 unitIDs = phy_extractor.get_unit_ids()
 try:
@@ -111,7 +135,10 @@ for col_to_del in ['ch', 'group', 'chan_group', 'depth', 'sh']:
 metadata = phy_interface.get_metadata()
 metadata["NWBFile"].update(session_start_time=session_start_time)
 
+# phy_interface.run_conversion(nwbfile_path=nwb_processed_outfile,
+#                              overwrite=False,
+#                              metadata=metadata,
+#                              **write_options)
 phy_interface.run_conversion(nwbfile_path=nwb_processed_outfile,
                              overwrite=False,
-                             metadata=metadata,
-                             **write_options)
+                             metadata=metadata)
